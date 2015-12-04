7649d32ee6cbc9fa1d10fd96c70c43c410ca02ce
Samuel William Reinhardt <swr@outlook.com>
2015-12-04 11:06:49 -0500

    fix yo problem

--- bin/countCode.php
+++ bin/countCode.php
@@ -3,7 +3,7 @@ function countify($code){
 		$code = strtoupper($code);
 		$counts = array('where'=>0,'condition'=>0,'quote'=>0,'symbol'=>0);
 		$counts['where'] = substr_count($code,'WHERE'); //counting all values bob's code requires (since we already case forced). Kinda cheating his system actually, but otherwise can't do robust dynamic queries.
-		$conditions = [' AND ', ' OR ', ' XOR ', ' NOT ', ' && ', ' || ', ' ! '];
+		$conditions = array(' AND ', ' OR ', ' XOR ', ' NOT ', ' && ', ' || ', ' ! ');
 		$counts['condition'] = 0;
 		foreach($conditions as $condition){$counts['condition'] += substr_count($code,$condition);} //substr_count doesn't accept arrays... boo
 		$quotes = ['"', "'", '#34', '#39', '&QUOT'];
