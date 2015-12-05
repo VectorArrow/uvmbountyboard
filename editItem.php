fe2fb90a1daadb78b0b9127277e032ff442dd252
Mike <mcardoso@uvm.edu>
2015-12-05 18:53:32 -0500

    im dumb

---
+++ editItem.php
@@ -0,0 +1,67 @@
+<?php include('parts/head.php'); ?>
+<body>
+<?php require_once('parts/header.php'); ?>
+<main>
+<?php
+  $whitelisted = true;
+  $id = preg_replace("/[^0-9]/","",htmlspecialchars($_GET["id"]));
+  if ($id === "") $whitelisted = false;
+  if ($whitelisted):
+  $query = 'SELECT * from Items WHERE id='.$id;
+  include('parts/grunt_work.php');
+?>
+<section class='content'>
+  <?php foreach ($results as $row){?>
+  <form action='updated.php' method='post'>
+  	<h2>Submit a Lost or Found Item</h2>
+  	<fieldset>
+  		<p>
+  		<label for='name'>Item Name:</label>
+  			<input type='text' name='name' >
+  		</p>
+  		<p>
+  		<label for='dateLost'>Date Lost/Found:</label>
+  			<input type='date' name='dateLost'>
+  		</p>
+  		<p>
+  		<label for='location'>Location:</label>
+  			<select id='location' name='location'>
+  				<option value='Athletic-Campus'>Athletic Campus</option>
+  				<option value='Centennial-Field'>Centennial Field</option>
+  				<option value='Central-Campus'>Central Campus</option>
+  				<option value='East-Annex'>East Annex</option>
+  				<option value='Medical'>Medical</option>
+  				<option value='Redstone-Campus'>Redstone Campus</option>
+  				<option value='University-Heights'>University Heights</option>
+  				<option value='University-Green'>University Green</option>
+  				<option value='Other'>Other</option>
+  			</select>
+  		</p>
+  		<p>
+  		<label for='description'>Description:</label>
+  			<textarea id='description' rows='8' cols='50' name='description'></textarea>
+  		</p>
+  		<p>
+  		<label for='category'>Categories:</label>
+  			<fieldset id='category'>
+  				<label><input type='checkbox' name='category' value='Pets'>Pet</label>
+  				<label><input type='checkbox' name='category' value='Phones'>Phone</label>
+  				<label><input type='checkbox' name='category' value='Bikes'>Bike</label>
+  				<label><input type='checkbox' name='category' value='Clothing'>Clothing</label>
+  				<label><input type='checkbox' name='category' value='Other'>Other</label>
+  			</fieldset>
+  		</p>
+  		<label for='status'>Status:</label>
+                          <fieldset id='status'>
+                                  <label><input type='radio' name='status' value='Lost' checked>Lost</label>
+                                  <label><input type='radio' name='status' value='Found'>Found</label>
+                          </fieldset>
+                  </p>
+  </fieldset>
+  	<input type="submit">
+  </form>
+  	<?php } ?>
+  </section>
+  </main>
+  </body>
+  </html>
---
+++ updated.php
@@ -0,0 +1,33 @@
+<?php include('parts/head.php'); ?>
+<body>
+<?php require_once('parts/header.php'); ?>
+<main>
+<section class='content'>
+<?php
+	$whitelisted = true;
+	print_r($data);
+	if ($whitelisted):
+
+		//$query = 'INSERT IGNORE INTO Users (username,nickname,email) VALUES ("'.$username.'","'.$username.'","'.$username.'@uvm.edu")';
+		//include(
+		$data = array(
+                	$username,
+                	htmlentities($_POST['name']),
+                	date('Y-m-d'),
+                	htmlentities($_POST['dateLost']),
+                	htmlentities($_POST['category']),
+               		htmlentities($_POST['description']),
+                	htmlentities($_POST['location']),
+                	htmlentities($_POST['status'])
+          	);
+		$query = 'INSERT INTO Items (username,name,dateCreated,dateLost,category,description,location,status) VALUES (?,?,?,?,?,?,?,?)';
+		require_once('parts/submit_grunt_work.php');
+		$primaryKey = $thisDatabaseWriter->lastInsert(); ?>
+	<h3>Your item has been submitted.</h3><p>You can view it <a href='item.php?id=<?php echo $primaryKey ?>'>here</a>.</p>
+<?php	else: ?>
+	<p>Basic security checks were not passed</p>
+<?php endif; ?>
+</section>
+</main>
+</body>
+</html>
