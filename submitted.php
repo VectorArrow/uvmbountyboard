8964456fd77d87fe9750be18e23ffc5206e63070
Mike <mcardoso@uvm.edu>
2015-12-05 20:28:29 -0500

    looks like the editing and database updating is all set

--- submitted.php
+++ submitted.php
@@ -9,18 +9,16 @@
 	if ($whitelisted):
 		if ($_POST['id'] != null):
 				$data = array(
-		                	$username,
 		                	htmlentities($_POST['name']),
-		                	date('Y-m-d'),
-		                	htmlentities($_POST['dateLost']),
 		                	htmlentities($_POST['category']),
 		               		htmlentities($_POST['description']),
 		                	htmlentities($_POST['location']),
-		                	htmlentities($_POST['status'])
+		                	htmlentities($_POST['status']),
+											htmlentities($_POST['id'])
 		          	);
-								$query = 'UPDATE Items SET name=?,category=?,description=?,location=?,status=? WHERE 1';
-								require_once('parts/submit_grunt_work.php');
-								$primaryKey = $thisDatabaseWriter->lastInsert();?>
+								$query = 'UPDATE Items SET name=?,category=?,description=?,location=?,status=? WHERE id=?';
+								require_once('parts/update_grunt_work.php');
+								$primaryKey = $_POST['id'];?>
 								<h3>Your item has been edited.</h3><p>You can view it <a href='item.php?id=<?php echo $primaryKey ?>'>here</a>.</p>
 		<?php else:
 
